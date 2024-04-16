9634f77d55eaa162d8dd8a06f724b4184d826d47
Eli Bradley <elibradley@utexas.edu>
2024-04-15 21:36:14 -0500

    handle single chapter books

--- download.sh
+++ download.sh
@@ -1,16 +1,24 @@
 #!/usr/bin/env bash
 
 KEY=$(<apikey.txt)
+find . -type f -name '*.html' -empty -print -delete
 while IFS=, read -r _ _ BOOK CHAPTERS _; do
     for i in $(seq 1 "$CHAPTERS")
     do
         mkdir -p "$BOOK"
-        FILE="$BOOK/$BOOK $i.html"
+        if [[ "$CHAPTERS" != "1" ]]
+        then
+            QUERY="$BOOK $i"
+        else
+            QUERY="$BOOK"
+        fi
+        FILE="$BOOK/$QUERY.html"
+
         if [ ! -f "$FILE" ]
         then
             sleep 1
             echo "Getting $FILE"
-            curl -H "Authorization: Token $KEY" -G "https://api.esv.org/v3/passage/html/" --data-urlencode "q=$BOOK+$i" --data-urlencode "include-audio-link=false" | jq -r .passages[] > "$FILE"
+            curl -H "Authorization: Token $KEY" -G "https://api.esv.org/v3/passage/html/" --data-urlencode "q=$QUERY" --data-urlencode "include-audio-link=false" | jq -r .passages[] > "$FILE"
         fi
     done
 done < <(tail -n +2 Books.csv)
