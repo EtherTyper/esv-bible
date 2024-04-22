1c1b90127c733f0e0a6fb81c30dab1d69914f641
Eli Bradley <elibradley@utexas.edu>
2024-04-22 14:57:03 -0500

    Add server code

---
+++ serve.py
@@ -0,0 +1,12 @@
+from http import server
+
+class UnicodeHTTPRequestHandler(server.SimpleHTTPRequestHandler):
+    def guess_type(self, path):
+        guess = server.SimpleHTTPRequestHandler.guess_type(self, path)
+        if guess == 'text/html':
+            return 'text/html; charset=utf-8'
+        else:
+            return guess
+
+httpd = server.HTTPServer(('', 8000), UnicodeHTTPRequestHandler)
+httpd.serve_forever()
